5a08353558196c2eaa1dde66e3aebb2322708cfa
group004 <shwetapal9158@gmail.com>
2019-06-15 07:47:31 +0530

    submitted

---
+++ src/main/java/com/cdac/question/Quetion1/Quetion1Application.java
@@ -0,0 +1,13 @@
+package com.cdac.question.Quetion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quetion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quetion1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdac/question/Quetion1/Quetion1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cdac.question.Quetion1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Quetion1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
